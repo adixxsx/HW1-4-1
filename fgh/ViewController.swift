620ee89d67924e7a4f9c13e1260952575f973bb4
adixxsx <ad1letsa5irkulov@gmail.com>
2023-12-08 21:35:11 -0800

    Initial Commit

---
+++ fgh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fgh
+//
+//  Created by user on 8/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
